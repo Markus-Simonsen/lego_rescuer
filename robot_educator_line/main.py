c3034addc2218a2975fbcdce36d023d264ce0d60
Markus Simonsen <markus.k.simonsen@gmail.com>
2024-09-25 13:49:15 +0200

    wrote new programs, hopefully they work

--- robot_educator_line/main.py
+++ robot_educator_line/main.py
@@ -21,18 +21,16 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 # Initialize the color sensor.
-line_sensor = ColorSensor(Port.S3)
+color_sensor_left = ColorSensor(Port.S3)
+color_sensor_right = ColorSensor(Port.S4)
 
 # Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
 
 # Calculate the light threshold. Choose values based on your measurements.
 BLACK = 9
 WHITE = 85
 threshold = (BLACK + WHITE) / 2
-
-# Set the drive speed at 100 millimeters per second.
-DRIVE_SPEED = 100
 
 # Set the gain of the proportional line controller. This means that for every
 # percentage point of light deviating from the threshold, we set the turn
@@ -40,18 +38,33 @@ DRIVE_SPEED = 100
 
 # For example, if the light value deviates from the threshold by 10, the robot
 # steers at 10*1.2 = 12 degrees per second.
-PROPORTIONAL_GAIN = 1.2
 
 # Start following the line endlessly.
 while True:
-    # Calculate the deviation from the threshold.
-    deviation = line_sensor.reflection() - threshold
 
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
-
-    # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
-
-    # You can wait for a short time or do other things in this loop.
-    wait(10)
+    # check if the left sensor sees anything
+    if (
+        color_sensor_left.reflection() < threshold
+        and color_sensor_right.reflection() > threshold
+    ):
+        left_motor.dc(25)
+        right_motor.dc(-25)
+    # check if the right sensor sees anything
+    if (
+        color_sensor_right.reflection() < threshold
+        and color_sensor_left.reflection() > threshold
+    ):
+        left_motor.dc(-25)
+        right_motor.dc(25)
+    # if both sensors sees something
+    elif (
+        color_sensor_right.reflection() < threshold
+        and color_sensor_left.reflection() < threshold
+    ):
+        left_motor.dc(-25)
+        right_motor.dc(-25)
+        wait(500)
+    # if both sensors does not see anything
+    else:
+        left_motor.dc(25)
+        right_motor.dc(25)
